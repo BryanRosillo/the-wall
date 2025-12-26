9a0b119a2fc26a7e89df571fe73ec730076e8401
BryanRosillo <bryanrosillo100@hotmail.com>
2025-12-25 20:01:48 -0500

    test(domain): new unit tests for phrase creation

--- backend/phrase-service/tests/PhraseService.UnitTests/DomainTests/PhraseTests.cs
+++ backend/phrase-service/tests/PhraseService.UnitTests/DomainTests/PhraseTests.cs
@@ -6,4 +6,17 @@ public class PhraseTests
         var phrase = new Phrase("Hello, world!");
         Assert.Equal("Hello, world!", phrase.Text);
     }
+
+    [Fact]
+    public void Create_phrase_with_empty_text_throws_exception()
+    {
+        Assert.Throws<NullTextException>(() => new Phrase(""));
+    }
+
+    [Fact]
+    public void Create_phrase_with_501_characters_text_throws_exception()
+    {
+        var longText = new string('a', 501);
+        Assert.Throws<TextLongException>(() => new Phrase(longText));
+    }
 }
