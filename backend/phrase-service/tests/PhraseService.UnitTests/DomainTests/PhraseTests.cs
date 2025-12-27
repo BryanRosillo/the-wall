60a44c31834755343cac57af5a4cb818a3851e4b
BryanRosillo <bryanrosillo100@hotmail.com>
2025-12-26 21:01:56 -0500

    test(domain): update of phrase unit tests

--- backend/phrase-service/tests/PhraseService.UnitTests/DomainTests/PhraseTests.cs
+++ backend/phrase-service/tests/PhraseService.UnitTests/DomainTests/PhraseTests.cs
@@ -1,25 +1,33 @@
 using PhraseService.Domain.Entities;
+using PhraseService.Domain.ValueObjects;
 using PhraseService.Domain.Exceptions;
 
 public class PhraseTests
 {
     [Fact]
-    public void Create_phrase_with_valid_text()
+    public void Create_phrase_with_valid_text_and_style()
     {
-        var phrase = new Phrase("Hello, world!");
+        var style = new Style("Arial", 10, "#0000FF");
+        var phrase = new Phrase("Hello, world!", style);
+
         Assert.Equal("Hello, world!", phrase.Text);
+        Assert.Equal(style, phrase.Style);
     }
 
     [Fact]
     public void Create_phrase_with_empty_text_throws_exception()
     {
-        Assert.Throws<NullTextException>(() => new Phrase(""));
+        var style = new Style("Arial", 10, "#0000FF");
+
+        Assert.Throws<NullTextException>(() => new Phrase("", style));
     }
 
     [Fact]
     public void Create_phrase_with_501_characters_text_throws_exception()
     {
         var longText = new string('a', 501);
-        Assert.Throws<TextLongException>(() => new Phrase(longText));
+        var style = new Style("Arial", 10, "#0000FF");
+
+        Assert.Throws<TextLongException>(() => new Phrase(longText, style));
     }
 }
