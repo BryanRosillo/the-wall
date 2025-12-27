8d4cc22d638b8ef52228cb72f9188e417d61d83a
BryanRosillo <bryanrosillo100@hotmail.com>
2025-12-26 21:04:42 -0500

    feat: creating a link between phrase and style

--- backend/phrase-service/src/PhraseService.Domain/Entities/Phrase.cs
+++ backend/phrase-service/src/PhraseService.Domain/Entities/Phrase.cs
@@ -1,12 +1,14 @@
 using PhraseService.Domain.Exceptions;
+using PhraseService.Domain.ValueObjects;
 
 namespace PhraseService.Domain.Entities
 {
     public class Phrase
     {
         public string Text { get; private set; }
+        public Style Style { get; private set; }
 
-        public Phrase(string text)
+        public Phrase(string text, Style style)
         {
             string textToAdd = text.Trim();
             if (textToAdd == "")
@@ -17,6 +19,7 @@ namespace PhraseService.Domain.Entities
             {
                 throw new TextLongException();
             }
+            Style = style;
             Text = textToAdd;
         }
 
