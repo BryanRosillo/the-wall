2fa9a412b35e9f4a9e8506983f1e0de4a36e6e35
BryanRosillo <bryanrosillo100@hotmail.com>
2025-12-29 17:55:45 -0500

    feat: author id property added to the Phrase entity

--- backend/phrase-service/src/PhraseService.Domain/Entities/Phrase.cs
+++ backend/phrase-service/src/PhraseService.Domain/Entities/Phrase.cs
@@ -6,11 +6,13 @@ namespace PhraseService.Domain.Entities
     public class Phrase
     {
         public string Text { get; private set; }
+        public string AuthorId {get; private set;}
         public Style Style { get; private set; }
 
-        public Phrase(string text, Style style)
+        public Phrase(string text, string authorId, Style style)
         {
             string textToAdd = text.Trim();
+            string author = authorId.Trim();
             if (textToAdd == "")
             {
                 throw new NullTextException();
@@ -19,6 +21,12 @@ namespace PhraseService.Domain.Entities
             {
                 throw new TextLongException();
             }
+            if(author == "")
+            {
+                throw new NullAuthorIdException();
+            }
+            
+
             Style = style;
             Text = textToAdd;
         }
