9d287e3bbd08640062d54bd3067c3b9925a4e0d3
BryanRosillo <bryanrosillo100@hotmail.com>
2025-12-30 18:39:42 -0500

    feat(bdd): publish a phrase successfully scenario passing end-to-end

--- backend/phrase-service/tests/PhraseService.BddTests/Support/PhraseRequest.cs
+++ backend/phrase-service/tests/PhraseService.BddTests/Support/PhraseRequest.cs
@@ -1,6 +1,7 @@
 public class PhraseRequest
 {
     public string Text { get; set; }
+    public string UserId { get; set; }
     public string Font { get; set; }
     public int FontSize { get; set; }
     public string Color { get; set; }
