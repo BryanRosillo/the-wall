77af4c444c480cb76e60c4063a1b4aef3d041fda
BryanRosillo <bryanrosillo100@hotmail.com>
2025-12-29 20:31:36 -0500

    feat: data persistence in memory added

---
+++ backend/phrase-service/src/PhraseService.Infrastructure/Persistence/InMemoryPhraseRepository.cs
@@ -0,0 +1,16 @@
+using PhraseService.Application.Ports;
+using PhraseService.Domain.Entities;
+
+namespace PhraseService.Infrastructure.Persistence
+{
+    public class InMemoryPhraseRepository : IPhraseRepository
+    {
+        private readonly List<Phrase> _phrases = new ();
+        public Task SaveAsync(Phrase phrase)
+        {
+            Phrases.Add(phrase);
+            return Task.CompletedTask;
+        }
+        
+    }
+}
