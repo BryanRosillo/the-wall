25d83adfaf3b173121d626ad769497b591ce4013
BryanRosillo <bryanrosillo100@hotmail.com>
2025-12-29 19:49:51 -0500

    feat: null value control

--- backend/phrase-service/src/PhraseService.Application/UseCases/PublishPhrase/PublishPhraseUseCase.cs
+++ backend/phrase-service/src/PhraseService.Application/UseCases/PublishPhrase/PublishPhraseUseCase.cs
@@ -15,6 +15,15 @@ namespace PhraseService.Application.UseCases.PublishPhrase
 
         public async Task Execute(PublishPhraseCommand command)
         {
+            if(command.Text is null)
+                throw new ArgumentNullException(nameof(command.Text));
+
+            if(command.UserId is null)
+                throw new ArgumentNullException(nameof(command.UserId));
+                
+            if(command.Style is null)
+                throw new ArgumentNullException(nameof(command.Style));
+
             Phrase phrase = new Phrase(command.Text, command.UserId, command.Style);
 
             await _repository.SaveAsync(phrase);
